b2329066104dbf92f0897166f153d86b8d6ddc1a
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-12 06:35:26 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = (num1 + num2)
+  return sum
 end
 
-def subtraction(num1, num2)
 
+def subtraction(num1, num2)
+  difference = (num1 - num2)
+  return difference
 end
 
 def division(num1, num2)
-
+  quotient = (num1 / num2)
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = (num1 * num2)
+  return product
 end
 
 def modulo(num1, num2)
-
+  remainder = (num1 % num2)
+  return remainder
 end
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
