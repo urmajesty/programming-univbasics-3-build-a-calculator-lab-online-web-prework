9f977cde7c6c2c3bf8ff5f3f10c05cb52f2f6d82
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-12 06:25:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,17 +10,20 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  
+  quotient = (num1 / num2)
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = (num1 * num2)
+  return product
 end
 
 def modulo(num1, num2)
-
+  remainder = (num1 % num2)
+  return remainder
 end
 
 def square_root(num)
-
+  
 end
